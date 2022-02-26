710a3394ee22623f43f684786f4a2f8cadecf20b
Pavel Kirilin <win10@list.ru>
2022-02-26 18:41:25 +0400

    Added Content-Length header. (#46)
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- src/protocol/core/get_info.rs
+++ src/protocol/core/get_info.rs
@@ -1,6 +1,6 @@
-use actix_web::{web, HttpRequest, HttpResponse};
-
 use crate::errors::RustusError;
+use actix_web::{web, HttpRequest, HttpResponse};
+use futures::stream::empty;
 
 use crate::{RustusResult, State};
 
@@ -47,18 +47,20 @@ pub async fn get_file_info(
     }
     builder
         .no_chunking(file_info.offset as u64)
-        .insert_header(("Upload-Offset", file_info.offset.to_string()))
-        .insert_header(("Content-Length", file_info.offset.to_string()));
+        .insert_header(("Upload-Offset", file_info.offset.to_string()));
     // Upload length is known.
     if let Some(upload_len) = file_info.length {
-        builder.insert_header(("Upload-Length", upload_len.to_string()));
+        builder
+            .no_chunking(upload_len as u64)
+            .insert_header(("Content-Length", file_info.offset.to_string()))
+            .insert_header(("Upload-Length", upload_len.to_string()));
     } else {
         builder.insert_header(("Upload-Defer-Length", "1"));
     }
     if let Some(meta) = file_info.get_metadata_string() {
         builder.insert_header(("Upload-Metadata", meta));
     }
-    Ok(builder.finish())
+    Ok(builder.streaming(empty::<RustusResult<web::Bytes>>()))
 }
 
 #[cfg(test)]
